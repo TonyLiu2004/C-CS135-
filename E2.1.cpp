77f30b7c214e496b35ae8a4e16f38b4b6000f48c
TonyLiu <ltsplice@gmail.com>
2023-01-27 15:05:11 -0500

    added E2.1

---
+++ E2.1.cpp
@@ -0,0 +1,28 @@
+/*
+Author: Tony Liu
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: E2.10
+
+Enter gallons, fuel efficiency, and price. Returns cost per 100 miles and how many miles the car can go
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int gallons;
+    int fuelEff;
+    int price;
+
+    cout << "Enter gallons of gas in tank: " << endl;
+    cin >> gallons;
+    cout << "Enter fuel efficiency: " << endl;
+    cin >> fuelEff;
+    cout << "Enter price per gallon: " << endl;
+    cin >> price;
+
+    double total = (100/fuelEff) * price;
+    cout << "The cost per 100 miles is $" << total << endl;
+    cout << "The car can go " << fuelEff * gallons << " miles" << endl;
+
+    return 0;
+}
