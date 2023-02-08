d54eee5176c515ecd410fedbf5b40d1f2e650c3c
TonyLiu <ltsplice@gmail.com>
2023-02-08 13:46:42 -0500

    did more in lab4

---
+++ lab4/checkerboard3x3.cpp
@@ -0,0 +1,50 @@
+//Author: Tony Liu
+//Course: CSCI-135
+//Instructor: Genady Maryash
+//Assignment: Lab4 G
+
+//Enter width and height, returns a 3x3 checkerboard with the requested size
+#include <iostream>
+using namespace std;
+int main(){
+    int width;
+    int height;
+    cout << "Input width: ";
+    cin >> width;
+    cout << "Input height: ";
+    cin >> height;
+
+    bool alt = false;
+    for(int i=1;i<height+1;i++){
+        if(alt){
+            cout << "   ";
+        }
+        bool pSpace = false;
+        for(int j =0;j<width;j++){
+            if(j%6==0){
+                pSpace = false;
+            }else{
+                if(j%3==0){
+                    pSpace = true;
+                }
+            }
+            if(pSpace){
+                cout << " ";
+            }else{
+                cout << "*";  
+            }
+        }
+        if(i%6==0){
+            alt = false;
+            width +=3;
+        }else{
+            if(i%3==0){
+                alt = true;
+                width -=3;
+            }
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
