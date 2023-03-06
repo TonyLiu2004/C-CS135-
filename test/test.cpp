ae6742ecab250ffadda836e9b04d9d3c9e927051
TonyLiu <ltsplice@gmail.com>
2023-03-06 13:25:13 -0500

    pointers

--- test/test.cpp
+++ test/test.cpp
@@ -1,61 +1,15 @@
 #include <iostream>
-#include <string>
-#include <fstream>
 using namespace std;
-
-<<<<<<< HEAD
 int main(){
-    string g_words[10] = {"sw","wq","","w","wes"};
-    for(string x : g_words){
-        cout << x << endl;
+    int list[15] = {1,10,2,20,3,30,4,40,12,120};
+    int* p = list;
+    cout << *(p+15) << endl;
+    cout << *(p+0) << endl;
+    *(p+10) = 420;
+    cout << "---------------" << endl;
+    *p = 0;
+    for(int i = 0;i < 16;i++){
+        cout << *p << endl;
+        p++;
     }
-    cout << "--------------" << endl;
-    for(int i = 0;i < 5;i++){
-        if(g_words[i] == ""){
-            for(int x = i; x < 5;x++){
-                g_words[x] = g_words[x+1];
-            }
-        }
-    }
-    for(string x : g_words){
-        cout << x << endl;
-    }
-    return 0;
-=======
-void sort(int a[],int b);
-
-int main(){
-    ifstream fin("text.txt");
-    if(fin.fail()){
-        cout << "file cannot be opened";
-        exit(1);
-    }
-    string a;
-    while(fin >> a){
-        cout << a << endl;
-    }
-    cout << "------------------" << endl;
-    int l[] = {12,3,4,6,6,71,3,4,56,7,2,0};
-    int track = 0;
-    for(auto b : l){
-        track++;
-    }
-    sort(l,track);
-    for (int a : l){
-        cout << a << endl;
-    }
-   return 0;
-}
-
-void sort(int a[], int b){
-    for(int i = 0;i < b;i++){
-        for(int x = i;x < b;x++){
-            if(a[x] < a[i]){
-                int t = a[i];
-                a[i] = a[x];
-                a[x] = t;
-            }
-        }
-    }
->>>>>>> 3aedbc1a8b236bb01cded04000940a5ab14988d7
 }
