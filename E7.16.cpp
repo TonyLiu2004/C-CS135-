3112142ee6ad9353df04c2ecce31613d9bf452d8
TonyLiu <ltsplice@gmail.com>
2023-03-08 14:17:51 -0500

    started lab 7

---
+++ E7.16.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Tony Liu
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: E7.16
+
+double distance(Point a, Point b) computes the distance from a to b.
+*/
+#include <iostream>
+using namespace std;
+double distance 
+int main(){
+    return 0;
+}
