cacb63f7e0355df09f034bfd5443fa3f0e7545cc
TonyLiu <ltsplice@gmail.com>
2023-03-09 13:20:23 -0500

    did E7.16

--- E7.16.cpp
+++ E7.16.cpp
@@ -7,8 +7,27 @@ Assignment: E7.16
 double distance(Point a, Point b) computes the distance from a to b.
 */
 #include <iostream>
+#include <cmath>
 using namespace std;
-double distance 
+
+struct Point{
+    double x;
+    double y;
+};
+double distance(Point a, Point b);
 int main(){
+    struct Point p1;
+    p1.x = 1;
+    p1.y = 2;
+    struct Point p2;
+    p2.x = 12;
+    p2.y = 20;
+    cout << "Point 1: (" << p1.x << ", " << p1.y << ")\n";
+    cout << "Point 2: (" << p2.x << ", " << p2.y << ")\n";
+    cout << "Distance between Point 1 and Point 2: " << distance(p1,p2) << "\n";
     return 0;
+}
+
+double distance(Point a, Point b){
+    return sqrt(pow((b.x-a.x),2)+pow((b.y-a.y),2));
 }
