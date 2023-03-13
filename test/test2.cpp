aa243871aa3e271f65b6591e00da31fd267d47ba
TonyLiu <ltsplice@gmail.com>
2023-03-13 15:28:15 -0400

    test2 has cipher stuff for zamansky hw

--- test/test2.cpp
+++ test/test2.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int l[] = {12,16,10,9,8,7};
-    int* p = l;
-    *p++;
-    for(int i =0;i < 10;i++){
-        cout << i << endl;
+
+int getLocation(char a){
+    char alpha[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int i = 0;
+    for(char x : alpha){
+        if(x == a){
+            return i;
+        }
+        i++;
     }
-    cout << *p << endl;
+}
+
+std::string solve(std::string encrypted_string){
+    vector<int> count(26,0);
+    vector<double> freq ={8.2,1.5,2.8,4.3,13,2.2,2,6.1,7,0.15,0.77,4,2.4,6.7,7.5,1.9,0.095,6,6.3,9.1,2.8,0.98,2.4,0.15,2,0.074};
+    //char alpha[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    
+    for(char x : encrypted_string){
+        if(isalpha(x)){
+            count[getLocation(x)] +=1;
+        }
+    }
+    return "";
+}
+
+int main(){
+    cout << getLocation('z') << endl <<endl;
+    cout << solve("hello world");
+    return 0;
 }
