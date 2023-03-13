54816436a8a4fa5e88f4f0c3fbd781121008c571
TonyLiu <ltsplice@gmail.com>
2023-03-13 13:20:04 -0400

    did tests for pixelate and testing pixelate

--- test/test.cpp
+++ test/test.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
 int main(){
-    int h =2;
-    int w = 2;
+    int h =4;
+    int w = 4;
     int list[h][w];
     
-    int i=0;
+    int i=10;
     for(int row =0;row < h;row++){
         for(int col = 0;col < w;col++){
             list[row][col] = i;
-            i++;
+            i+=10;
             cout << list[row][col] << " ";
         }
+        i+=1;
+        i-=40;
         cout << endl;
     }
 
-
-    int list2[h*2][w*2];
-    for(int row =0;row < h*2;row++){
-        for(int col = 0;col < w*2;col++){
-            list2[row][col] = list[row/2][col/2];
+    int list2[h][w];
+    for(int row =0;row < h;row+=2){
+        for(int col = 0;col < w;col+=2){
+            int rounded = round((list[row][col] + list[row+1][col] + list[row][col+1] + list[row+1][col+1])/4 +0.5);
+            list2[row][col] = rounded;
+            list2[row+1][col] = rounded;
+            list2[row][col+1] = rounded;
+            list2[row+1][col+1] = rounded;
+        }
+    }
+    cout << "----------------------" << endl;
+    for(int row = 0;row < h;row++){
+        for(int col = 0;col < w;col++){
             cout << list2[row][col] << " ";
         }
         cout << endl;
     }
-    
+
 }
