8f51718c8376b6b3c85a8263584be5e29c495817
Tonyliu2004 <ltsplice@gmail.com>
2023-03-13 21:06:41 -0400

    did some test2

--- test/test2.cpp
+++ test/test2.cpp
@@ -3,6 +3,34 @@
 #include <cmath>
 using namespace std;
 
+string encryptCaesar(string plaintext, int rshift);
+
+char shiftChar(char c, int rshift){
+    char ret = c;
+    if(!isalpha(c)){
+        return c;
+    }
+    for(int i = 0;i < rshift;i++){
+        if(ret == 'z'){
+            ret = 'a';
+        }else if(ret == 'Z'){
+            ret = 'A';
+        }else{
+            ret+=1;
+        }
+    }
+    return ret;
+}
+
+string encryptCaesar(string plaintext, int rshift){
+    string ret = "";
+    for(char x : plaintext){
+        ret+=shiftChar(x,rshift);
+    }
+    return ret;
+}
+
+//returns the position of a letter in the alphabet, from 0-25
 int getLocation(char a){
     char alpha[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int i = 0;
@@ -14,36 +42,48 @@ int getLocation(char a){
     }
 }
 
-vector<double> dist(vector<int> count, vector<double> freq){
-    vector<double> ret(26,0);
+//distance formula is  sqrt(  (PhraseFreqA - EngFreqA)^2 + (Phrase FreqB - EngFreB)^2... etc)
+double dist(vector<int> count){
+    vector<double> freq ={8.2,1.5,2.8,4.3,13,2.2,2,6.1,7,0.15,0.77,4,2.4,6.7,7.5,1.9,0.095,6,6.3,9.1,2.8,0.98,2.4,0.15,2,0.074};
+    double sum = 0;
     int i = 0;
     for(double x : count){
-        ret[i] = pow(x - freq[i],2);
+        sum+=pow(count[i]-freq[i],2);
         i++;
     }
-    return ret;
+    return sqrt(sum);
 }
 
-std::string solve(std::string encrypted_string){
+//returns a vector with the frequency of each of the 26 letters of the alphabet in order
+vector<int> letterFreq(string a){
     vector<int> count(26,0);
-    vector<double> freq ={8.2,1.5,2.8,4.3,13,2.2,2,6.1,7,0.15,0.77,4,2.4,6.7,7.5,1.9,0.095,6,6.3,9.1,2.8,0.98,2.4,0.15,2,0.074};
-    //char alpha[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
-    for(char x : encrypted_string){
+    for(char x : a){
         if(isalpha(x)){
             count[getLocation(x)] +=1;
         }
     }
+    return count;
+}
 
-    vector<double> distance = dist(count,freq);
-    for(auto x : distance){
-        cout << x << endl;
+std::string solve(std::string encrypted_string){
+    string ret = "";
+    int sDistance = 100;
+    for(int i =0;i < 25;i++){
+        string r = encryptCaesar(encrypted_string,i);
+        vector<int> letterCount = letterFreq(r);
+        double distance = dist(letterCount);
+        if(distance < sDistance){
+            sDistance = distance;
+            ret = encryptCaesar(encrypted_string,i);
+        }
     }
-    return "";
+    return ret;
 }
 
 int main(){
-    cout << getLocation('z') << endl <<endl;
-    cout << solve("hello world");
+    string s = "Hello, World!";
+    string e = encryptCaesar(s,1);
+    cout << e << endl;
+    cout << solve("i love eating cookies"); // doesnt deal with caps and non-letters yet, fix
     return 0;
 }
