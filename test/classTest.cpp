815e6e4d3907685bf89fc8736aa427fbc4f2dcd0
TonyLiu <ltsplice@gmail.com>
2023-03-23 12:58:05 -0400

    added class test

--- test/classTest.cpp
+++ test/classTest.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 class Counter{
     public:
+    void reset();
     void add(int);
     void subtract(int);
     int get();
 
     private:
-    int count = 0;
+    int count;
 };
 
+void Counter::reset(){
+    count = 0;
+}
 void Counter::add(int a){
     count+=a;
 }
@@ -24,6 +28,7 @@ int Counter::get(){
 
 int main(){
     Counter c;
+    c.reset();
     c.add(10);
     cout << c.get() << "\n";
     c.add(10);
