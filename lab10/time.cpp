b57ecf668751a57a3e921ddf66095c1285bfcb98
TonyLiu <ltsplice@gmail.com>
2023-03-23 15:15:33 -0400

    did some lab10

--- lab10/time.cpp
+++ lab10/time.cpp
@@ -14,9 +14,11 @@ void printTime(Time time) {
 
 int minutesSinceMidnight(Time time);
 int minutesUntil(Time earlier, Time later);
+Time addMinutes(Time time0, int min);
 
 
 int main(){
+    /*
     int h1;
     int m1;
     int h2;
@@ -29,6 +31,13 @@ int main(){
     Time t2 = {h2,m2};
     cout << minutesSinceMidnight(t1) << endl;
     cout << minutesUntil(t1,t2) << endl;
+    */
+    Time t3 = {8,10};
+    Time t4 = addMinutes(t3,75); 
+    Time t5 = {24,10};
+    Time t6 = addMinutes(t5,50);
+    cout << t4.h << " " << t4.m << endl;
+    cout << t6.h << " " << t6.m << endl;
     return 0;
 }
 
@@ -37,4 +46,19 @@ int minutesSinceMidnight(Time time){
 }
 int minutesUntil(Time earlier, Time later){
     return (abs(earlier.h - later.h) * 60) + abs(earlier.m - later.m);
+}
+
+Time addMinutes(Time time0, int min){
+    int minutes = min%60; // 
+    int hours = min/60;
+    time0.h +=hours;
+    time0.m +=minutes;
+    if(time0.m == 60){
+        time0.m = 0;
+        time0.h++;
+    }
+    if(time0.h == 25){
+        time0.h = 0;
+    }
+    return {time0.h,time0.m};
 }
