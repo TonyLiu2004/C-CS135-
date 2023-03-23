de882a1e1b07a633817337ca39b4fc3a055d45ea
TonyLiu <ltsplice@gmail.com>
2023-03-23 16:02:52 -0400

    did more changes

--- lab10/time.cpp
+++ lab10/time.cpp
@@ -2,20 +2,46 @@
 #include <cmath>
 using namespace std;
 
+enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
+
 class Time { 
 public:
     int h;
     int m;
 };
 
+class Movie { 
+public: 
+    string title;
+    Genre genre;     // only one genre per movie
+    int duration;    // in minutes
+};
+
+class TimeSlot { 
+public: 
+    Movie movie;     // what movie
+    Time startTime;  // when it starts
+};
+
 void printTime(Time time) {
     cout << time.h << ":" << time.m;
+}
+
+void printMovie(Movie mv){
+    string g;
+    switch (mv.genre) {
+        case ACTION   : g = "ACTION"; break;
+        case COMEDY   : g = "COMEDY"; break;
+        case DRAMA    : g = "DRAMA";  break;
+        case ROMANCE  : g = "ROMANCE"; break;
+        case THRILLER : g = "THRILLER"; break;
+    }
+    cout << mv.title << " " << g << " (" << mv.duration << " min)";
 }
 
 int minutesSinceMidnight(Time time);
 int minutesUntil(Time earlier, Time later);
 Time addMinutes(Time time0, int min);
-
 
 int main(){
     /*
@@ -31,13 +57,22 @@ int main(){
     Time t2 = {h2,m2};
     cout << minutesSinceMidnight(t1) << endl;
     cout << minutesUntil(t1,t2) << endl;
-    */
+    
+    //testing addMinutes
     Time t3 = {8,10};
     Time t4 = addMinutes(t3,75); 
     Time t5 = {24,10};
     Time t6 = addMinutes(t5,50);
     cout << t4.h << " " << t4.m << endl;
     cout << t6.h << " " << t6.m << endl;
+    */
+    Movie movie1 = {"Back to the Future", COMEDY, 116};
+    Movie movie2 = {"Black Panther", ACTION, 134};
+
+    TimeSlot morning = {movie1, {9, 15}};  
+    TimeSlot daytime = {movie2, {12, 15}}; 
+    TimeSlot evening = {movie2, {16, 45}}; 
+    printMovie(movie1);
     return 0;
 }
 
