55be2184d16ee6f6293dd24e2bbad3cebc6e1c86
TonyLiu <ltsplice@gmail.com>
2023-04-21 14:31:20 -0400

    j

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+/*
+Author: Tony Liu
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: E3.1
+
+Input a number, code prints whether the number is positive, zero, or negative
+*/
+#include <iostream>
+using namespace std;
+int main(){
