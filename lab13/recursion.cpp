d022bed5b28957c92b52f3f4f3c49404d0531f44
TonyLiu <ltsplice@gmail.com>
2023-05-01 15:36:12 -0400

    started lab 13

---
+++ lab13/recursion.cpp
@@ -0,0 +1,74 @@
+/*
+Author: Tony Liu
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Lab13
+
+Recursion lab
+*/
+#include <iostream>
+using namespace std;
+
+//Prints all numbers from range left to right
+void printRange(int left, int right){
+    if(left <= right){
+        cout << left << " ";
+        printRange(left+1,right);
+    }
+}
+
+//computes the sum of all numbers in range
+int sumRange(int left, int right){
+    if(left > right){ // base case
+        return 0;
+    }
+    return left + sumRange(left+1,right);
+}
+
+int sumArray(int *arr, int size){
+    if(size<1){
+        return 0;
+    }
+    return arr[size-1] + sumArray(arr,size-1);
+}
+
+bool isAlphanumeric(string s){
+    if(isalnum(s[0])){
+        return false;
+    }
+    return true;
+}
+int main(){
+    //Task A
+    cout << "Print Range: ";
+    printRange(-2,10);
+    cout << "\n-----------\n";
+
+    //Task B
+    cout << "sum of range: ";
+    int x = sumRange(1, 3);
+    int y = sumRange(-2, 10);
+    cout << x << " " << y;
+    cout << "\n------------\n";
+
+    //Task C
+    cout << "sum array: \n";
+    int *arr = new int[10];
+    arr[0] = 12;
+    arr[1] = 17;
+    arr[2] = -5;
+    arr[3] = 3;
+    arr[4] = 7;
+    arr[5] = -15;
+    arr[6] = 27;
+    arr[7] = 5;
+    arr[8] = 13;
+    arr[9] = -21;
+    cout << sumArray(arr,10) << "\n"; //43
+    cout << sumArray(arr, 5) << "\n"; // sum of first 5, 34
+    cout << "-----------------\n";
+
+    //Task D
+    
+    return 0;
+}
